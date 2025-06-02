a8833b9682a19cbf260db369d78f95b14060e94b
elnatnael <meranat9104@gmail.com>
2025-06-02 23:36:10 +0300

    Memory-Efficient Aggregation with Generators

--- python-generators-0x00/4-stream_ages.py
+++ python-generators-0x00/4-stream_ages.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python3
 seed = __import__('seed')
 
-def stream_user_ages(batch_size=100):
+def stream_user_ages():
     offset = 0
+    batch_size = 100
     while True:
         conn = seed.connect_to_prodev()
         cursor = conn.cursor(dictionary=True)
@@ -20,20 +20,16 @@ def stream_user_ages(batch_size=100):
                 continue
 
         offset += batch_size
-
 def compute_average_age():
-    total_age = 0
+    total = 0
     count = 0
-
     for age in stream_user_ages():
-        total_age += age
+        total += age
         count += 1
-
     if count == 0:
         print("No valid age data found.")
     else:
-        average = total_age / count
-        print(f"Average age of users: {average:.2f}")
+        print(f"Average age of users: {total / count:.2f}")
 
 if __name__ == "__main__":
     compute_average_age()
