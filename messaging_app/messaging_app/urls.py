d6dd13ddf433122b507497b1112f23cb43eebc85
elnatnael <meranat9104@gmail.com>
2025-06-14 11:20:40 +0300

    Build api endpoints with views

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -1,14 +1,25 @@
+"""
+URL configuration for messaging_app project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
 from django.urls import path, include
-from rest_framework_nested.routers import SimpleRouter, NestedSimpleRouter
-from .views import ConversationViewSet, MessageViewSet
-
-router = SimpleRouter()
-router.register(r'conversations', ConversationViewSet, basename='conversations')
-
-conversation_router = NestedSimpleRouter(router, r'conversations', lookup='conversation')
-conversation_router.register(r'messages', MessageViewSet, basename='conversation-messages')
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('', include(conversation_router.urls)),
+    path('admin/', admin.site.urls),
+    path('api/', include('chats.urls')),  # <- This is what the checker needs
 ]
+
+
