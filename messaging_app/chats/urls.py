9e2fa9a921cf4b7395a1e3078f9bb0e8149cad79
elnatnael <meranat9104@gmail.com>
2025-06-14 11:11:38 +0300

    Build api endpoints with views

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,15 +1,13 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework import routers
 from rest_framework_nested.routers import NestedDefaultRouter
 from .views import ConversationViewSet, MessageViewSet
 
-
-router = DefaultRouter()
+router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversations')
 
 conversation_router = NestedDefaultRouter(router, r'conversations', lookup='conversation')
 conversation_router.register(r'messages', MessageViewSet, basename='conversation-messages')
-
 
 urlpatterns = [
     path('', include(router.urls)),
