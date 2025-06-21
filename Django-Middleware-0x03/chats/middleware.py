dc3bbf65912b4f7266c9128b5bba98b49b3bead8
elnatnael <meranat9104@gmail.com>
2025-06-22 00:54:45 +0300

    Restrict Chat Access by time

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,7 +1,6 @@
-# chats/middleware.py
 from datetime import datetime
-import os
 from django.http import HttpResponseForbidden
+import os
 
 class RequestLoggingMiddleware:
     def __init__(self, get_response):
@@ -11,11 +10,9 @@ class RequestLoggingMiddleware:
         username = request.user.get_username() if hasattr(request, 'user') and request.user.is_authenticated else 'Anonymous'
         log_message = f"{datetime.now()} - User: {username} - Path: {request.method} {request.path}\n"
 
-        # This resolves to your project root
         base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         log_path = os.path.join(base_dir, 'requests.log')
 
-        # 🧠 PRINT FOR DEBUGGING
         print("📄 Attempting to log to:", log_path)
 
         try:
@@ -25,9 +22,7 @@ class RequestLoggingMiddleware:
             print(f"❌ Error writing log: {e}")
 
         return self.get_response(request)
-    
 
-    
 class RestrictAccessByTimeMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
@@ -37,7 +32,3 @@ class RestrictAccessByTimeMiddleware:
         if current_hour < 18 or current_hour > 21:
             return HttpResponseForbidden("Access denied: Chat allowed only between 6 PM and 9 PM.")
         return self.get_response(request)
-
-
-    
-
