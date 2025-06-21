66e35f699339076215812186cdea0e53bcb7e81c
elnatnael <meranat9104@gmail.com>
2025-06-22 01:26:08 +0300

    Restrict Chat Access by time and Detect and Block offensive Language1

--- Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/settings.py
@@ -49,18 +49,18 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
 
 ]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    
-    'chats.middleware.RestrictAccessByTimeMiddleware',  # Moved BEFORE auth
-    
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'chats.middleware.RequestLoggingMiddleware',  # Keeps user info access
-    
+
+    'chats.middleware.RequestLoggingMiddleware',
+    'chats.middleware.RestrictAccessByTimeMiddleware',  # <- this one
+
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
