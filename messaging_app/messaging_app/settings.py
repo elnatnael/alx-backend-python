6e7de71fa3524505566b3ef833ac33e5c2ab97e4
elnatnael <meranat9104@gmail.com>
2025-07-19 17:16:09 +0300

    Set Up a GitHub Actions Workflow for Testing

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -104,8 +104,12 @@ WSGI_APPLICATION = 'messaging_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('DB_NAME', 'your_default_db_name'),
+        'USER': os.environ.get('DB_USER', 'your_default_user'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'your_default_password'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '3306'),
     }
 }
 
