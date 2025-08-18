47617c4d6a44ff43fea0b13f7a8b34118f1944b3
elnatnael <meranat9104@gmail.com>
2025-08-18 04:36:26 +0300

    added settings for task 3

--- Django-Middleware-0x03/chats/settings.py
+++ Django-Middleware-0x03/chats/settings.py
@@ -61,6 +61,7 @@ MIDDLEWARE = [
     'chats.middleware.RequestLoggingMiddleware',
     'chats.middleware.RolepermissionMiddleware',
     'chats.middleware.RestrictAccessByTimeMiddleware',
+    'chats.middleware.OffensiveLanguageMiddleware',
 
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
